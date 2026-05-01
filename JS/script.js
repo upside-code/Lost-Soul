89c771654f3d2deb97e43062cfcb7bc9339b36fb
Erik Gruber <upsidedown737@gmail.com>
2026-05-01 14:49:07 +0200

    added some loreum ipsum and contact form with js verification

--- JS/script.js
+++ JS/script.js
@@ -8,7 +8,35 @@ toggle.addEventListener('click', () =>
 });
 
 
-const lightbox = GLightbox(
-    {
+//const lightbox = GLightbox({});
 
-});
+
+
+function validateForm() {
+    let name = document.getElementById("name").value;
+    if (name === "") {
+        alert("Name must be filled out");
+        return false;
+    }
+    let email = document.getElementById("email").value;
+    if (email === "") {
+        alert("Email must be filled out");
+        return false;
+    }
+    let subject = document.getElementById("subject").value;
+    if (subject === "") {
+        alert("subject must be filled out");
+        return false;
+    }
+    let source = document.querySelector('input[name="source"]:checked');
+    if (!source) {
+        alert("source must be filled out");
+        return false;
+    }
+    let message = document.getElementById("message").value;
+    if (message === "") {
+        alert("message must be filled out");
+        return false;
+    }
+    return true;
+}
